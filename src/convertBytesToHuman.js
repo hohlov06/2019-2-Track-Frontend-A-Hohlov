5af8e7c14238e2b19a07ece3550da90e796d9966
lupa_or_pupa <hohlov_06@mail.ru>
2019-10-01 14:44:15 +0300

    functions&tests

---
+++ src/convertBytesToHuman.js
@@ -0,0 +1,52 @@
+/*
+ * В этом задании надо разработать функцию
+ * `convertBytesToHuman`. Эта функция  должна принимать
+ * аргумент `bytes` только числового типа.
+ * На выходе функция должна отдать
+ * человекопонятную строку, которая будет
+ * отражать размер файла. Примеры использования:
+ * `convertBytesToHuman(1024) === '1 KB';`
+ * `convertBytesToHuman(123123123) === '117.42 MB';`
+ * Необходимо предусмотреть защиту от
+ * передачи аргументов неправильного типа
+ * и класса (например, отрицательные числа)
+ */
+
+function bytesToHumanDecorator(){
+  let postfix = [' PB', ' TB', ' GB', ' MB', ' KB', ' B']
+  let bytesNumber = [];
+  let num = 1;
+  for (let i = 0; i < postfix.length; i++){
+    bytesNumber.push(num);
+    num *= 1024;
+  }
+  bytesNumber = bytesNumber.reverse();
+
+  return function(bytes){
+    let i = 0;
+    for (; i < bytesNumber.length - 1; i++)
+      if (bytes >= bytesNumber[i]){
+        let converted = bytes / bytesNumber[i];
+        let trunced = Math.trunc(converted);
+        converted = ((converted - trunced) < 1e-2)
+          ? trunced
+          : (Math.floor(converted * 100) / 100).toFixed(2);
+        return String(converted) + postfix[i];
+      }
+    return String(bytes) + postfix[i];
+  }
+}
+
+let convert = bytesToHumanDecorator();
+
+export default function convertBytesToHuman(bytes) {
+  if( (typeof(bytes) != 'number')
+    || isNaN(bytes)
+    || !isFinite(bytes)
+    || !Number.isInteger(bytes)
+    || bytes === null
+    || bytes < 0)
+    return false;
+  return convert(bytes);
+}
+
---
+++ src/nonUniqueElements.js
@@ -0,0 +1,42 @@
+/*
+You are given a non-empty list of integers (X).
+
+For this task, you should return a list consisting of
+only the non-unique elements in this list.
+
+To do so you will need to remove all unique elements
+(elements which are contained in a given list only once).
+
+When solving this task, do not change the order of the list.
+
+Example:
+
+input (array of integers): [1, 2, 3, 1, 3]
+output (iterable of integers): [1, 3, 1, 3]
+
+1 and 3 are non-unique elements.
+
+More examples:
+
+nonUniqueElements([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+nonUniqueElements([1, 2, 3, 4, 5]) == []
+nonUniqueElements([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+nonUniqueElements([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
+ */
+
+export default function nonUniqueElements(data) {
+  let nonUniqueSet = new Set();
+
+  for (let index = 0; index < data.length; index++){
+    let element = data[index];
+    if (!nonUniqueSet.has(element))
+      if (data.indexOf(element, index+1) != -1)
+        nonUniqueSet.add(element);
+      else{
+        data.splice(index,1);
+        index--;
+      }
+  }
+  return data;
+}
+
